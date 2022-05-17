665409fe9dbee145e687443e648037d422198c75
Nomanal22 <105661867+Nomanal22@users.noreply.github.com>
2022-05-17 21:25:49 +0600

    Add files via upload

---
+++ Mad Libs game.py
@@ -0,0 +1,21 @@
+
+'''
+color = input("Enter a color : ")
+plural_noun = input("Enter a plural noun : ")
+celabrity  = input("Enter a celabrity  : ")
+
+print("Rose are "+color)
+print(plural_noun+" are blue ")
+print("I love "+celabrity)
+
+'''
+
+adj = input("Adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("Famous person : ")
+
+madlibs = f"Computer programming is so {adj}! It makes me so exited all the time because\
+          I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+print(madlibs)
+
